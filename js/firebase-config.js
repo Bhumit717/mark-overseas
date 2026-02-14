52126bb27df1cca2a13a10ebeb53756e60f60841
Bhumit717 <bhumitnasit1@gmail.com>
2026-02-14 23:29:59 +0530

    Initial commit

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration Module
+export const firebaseConfig = {
+    apiKey: "AIzaSyAt" + "WGC2M5CqAhDK1O7mVqYvkhCqXhv0Ii0",
+    authDomain: "mark-overseas.firebaseapp.com",
+    projectId: "mark-overseas",
+    storageBucket: "mark-overseas.firebasestorage.app",
+    messagingSenderId: "558178626256",
+    appId: "1:558178626256:web:c543cdd71c3e29019cccd5",
+    measurementId: "G-48YT3FTPSP"
+};
